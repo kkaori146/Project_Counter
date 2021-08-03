466efbc152d8b2abf9afc192db46c73d41a32443
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-03 09:15:18 +0900

    emojis

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@ function calculate() {
     var ini = document.getElementById('txti')
     var fim = document.getElementById('txtf')
     var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert('[ERROR] Fill in the data!')
     } else {
-        alert('OK!')
+        res.innerHTML = 'Counting:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(var c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
